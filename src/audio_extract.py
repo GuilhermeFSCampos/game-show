42e19c300cccf6de89b9dc9ba17476c8cdcea5a4
gfscampos <gfscampos@bitbucket.org>
2017-06-30 21:45:27 -0400

    Fix UTF-8 setting on json dump.

--- src/audio_extract.py
+++ src/audio_extract.py
@@ -5,5 +5,5 @@ audios = []
 for f in glob.glob('../public/audios/*'):
     audios.append(f.split('/')[3])
 
-with open('audios.json', 'w') as out:
-    json.dump(audios, out)
+with open('audios.json', 'w', encoding='utf-8') as out:
+    json.dump(audios, out, ensure_ascii=False)
