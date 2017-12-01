171f34d8ee056a299360ccc886b77d3f1210727e
Guilherme Campos <guilhermefscampos@gmail.com>
2017-12-01 09:59:30 -0300

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Routes from './components/Routes';
+import logo from './logo.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>BibliaVest - Scoreboard</h2>
+          <img src={logo} className="App-logo2" alt="logo" />
         </div>
         <Routes />
       </div>
